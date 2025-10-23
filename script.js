5c72d4226d38664f20d177ea3f9acb7cef07df6b
partengo <thomasbarrerafuentes2006@gmail.com>
2025-10-23 14:39:33 -0300

    Crear script.js y conectarlo al HTML

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Script.js cargado correctamente');
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM completamente cargado');
+    
+    initHeaderColorChange();
+    initInteractiveButton();
+    initFormValidation();
+});
